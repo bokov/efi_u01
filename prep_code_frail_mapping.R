31e29320b18cc607cc8c545b3e66d3502653081d
Alex Bokov <bokov@uthscsa.edu>
2020-02-20 19:34:36 -0600

    prep_code_frail_mapping.R; bug; Fixed trailing periods

--- prep_code_frail_mapping.R
+++ prep_code_frail_mapping.R
@@ -31,16 +31,16 @@ panderOptions('p.copula',', and ');
 
 .currentscript <- current_scriptname('prep_code_frail_mapping.R');
 #' ### Clean up mapping table
-code2grpin
+#code2grpin
 code2grpout <- group_by(code2grpin,`No. `) %>% group_modify(function(xx,yy,...){
   with(xx,data.frame(diag=c(`ICD-10 Definition`,`ICD-9 Definition`) %>%
                        strsplit(',[ ]*') %>% unlist %>% sapply(ranges) %>%
-                       unlist %>% gsub('x$','',.)
+                       unlist %>% gsub('[.]?x[.]?$','',.)
                      ,deficit=Deficit[1],source=Source[1],notes=Notes[1]))}) %>%
   select(1:5);
 names(code2grpout)[1] <- 'grp';
 export(code2grpout,'local/out/code_to_efigrp.csv');
-with(code2grpout,paste0('\nunion select ',grp,", '",diag,"' from dual")) %>%
+with(code2grpout,paste0('\nunion select ',grp,", '",diag,"', '",deficit,"', '",source,"' from dual")) %>%
   cat(file='local/out/code_to_efigrp.sql');
 #'
 #'
